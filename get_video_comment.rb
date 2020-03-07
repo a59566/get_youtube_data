ad1edc21dfaa8749b24f87bb2fc14905081d2d0f
a59566 <a59566@outlook.com>
2020-03-07 13:01:36 +0800

    update command line options raw output to raw/ log output to log/ refactoring

--- get_video_comment.rb
+++ get_video_comment.rb
@@ -4,6 +4,18 @@ require 'bundler/setup'
 require 'google/apis/youtube_v3'
 require 'json'
 require 'optparse'
+require 'date'
+require 'uri'
+require 'logger'
+require 'benchmark'
+
+def write_json_file(file_path, json)
+  FileUtils.mkpath(File.dirname(file_path))
+
+  File.open(file_path, 'w:UTF-8') do |file|
+    file.write(JSON.pretty_generate(json))
+  end
+end
 
 def get_comment_properties(comment)
   item = {}
@@ -18,23 +30,8 @@ def get_comment_properties(comment)
   item
 end
 
-if $PROGRAM_NAME == __FILE__
-  # パラメータの解析
-  params = {}
-  OptionParser.new do |opts|
-    opts.on('-i', '--input VIDEO_ID', String, 'Youtube video id')
-    opts.on('-o', '--output-dir [DIR]', String, 'Output directory')
-  end.parse!(into: params)
-
-  # デフォルトフォルダ comment
-  video_id = params[:input]
-  output_dir = params[:"output-dir"] || 'comment'
-
-  # youtube api
-  service = Google::Apis::YoutubeV3::YouTubeService.new
-  service.key = File.read('youtube_api.key')
-
-  # 結果を取得する
+def get_comments(service, video_id)
+  # Youtube Data api でコメント取得する
   comment_threads = service.fetch_all do |token, s|
     s.list_comment_threads('snippet, replies', max_results: 100, video_id: video_id, page_token: token)
   end
@@ -45,8 +42,8 @@ if $PROGRAM_NAME == __FILE__
     item = get_comment_properties(comment_thread.snippet.top_level_comment)
 
     # リプライを処理する
-    # commentThread.list 取得できるリプライは五つまで
-    # 残りのは comment.list で取得する
+    # commentThread.list api で取得できるリプライは五つまで
+    # 残りのは comment.list api で取得する
     if comment_thread.snippet.total_reply_count != 0
       item[:replies] = []
 
@@ -68,17 +65,78 @@ if $PROGRAM_NAME == __FILE__
     raw_items[:raw_items].push(comment_thread.to_h)
   end
 
-  # 処理したは #{output_dir}/#{video_id}_comments.json に書き込む
-  # 未処理は  #{output_dir}/#{video_id}_raw_comments.json に書き込む
-  FileUtils.mkpath(output_dir)
+  { items: items, raw_items: raw_items }
+end
+
+def get_comments_process(service, video_id, output_dir)
+  comments = get_comments(service, video_id)
+
+  # 処理したは #{output_dir}/#{file_name_prefix}_comments.json に書き込む
+  # 未処理は  #{output_dir}/raw/#{file_name_prefix}_raw_comments.json に書き込む
   output_path = File.join(output_dir, "#{video_id}_comments.json")
-  raw_output_path = File.join(output_dir, "#{video_id}_raw_comments.json")
+  raw_output_path = File.join(output_dir, 'raw', "#{video_id}_raw_comments.json")
 
-  File.open(output_path, 'w:UTF-8') do |file|
-    file.write(JSON.pretty_generate(items))
-  end
+  write_json_file(output_path, comments[:items])
+  write_json_file(raw_output_path, comments[:raw_items])
 
-  File.open(raw_output_path, 'w:UTF-8') do |file|
-    file.write(JSON.pretty_generate(raw_items))
+  LOGGER.info { "get comment succeed, video_id: #{video_id}" }
+rescue StandardError => e
+  LOGGER.error { "#{e.class}: #{e.message}\n#{e.backtrace.join("\n")}" }
+end
+
+if $PROGRAM_NAME == __FILE__
+  # パラメータの解析
+  params = {}
+  OptionParser.new do |opts|
+    opts.on('-d', '--debug', 'Debug mode switch')
+    opts.on('-i', '--input INPUT', String, 'Allow 3 types input', '1. youtube video url',
+            '2. youtube video id', '3. output file of get_playlist_videos_info.rb')
+    opts.on('-o', '--output-dir [DIR]', String, 'Output directory, default is comment/')
+  end.parse!(into: params)
+
+  begin
+    # ログファイルの設定
+    # log/get_video_comment.log
+    log_file_path = File.join('log', "#{File.basename($PROGRAM_NAME, '.rb')}.log")
+    FileUtils.mkpath(File.dirname(log_file_path))
+    log_file = File.open(log_file_path, 'w:UTF-8')
+    log_file.sync = true
+    log_level = params[:debug] ? :debug : :info
+    LOGGER = Logger.new(log_file, level: log_level)
+    LOGGER.info { "params: #{params}, program start" }
+
+    # youtube api
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = File.read('youtube_api.key')
+
+    # デフォルトoutput_dirは comment/
+    input = params[:input]
+    output_dir = params[:"output-dir"] || 'comment'
+
+    # main process
+    elapsed_time = Benchmark.realtime do
+      # inputはファイル
+      if File.exist?(input)
+        playlist_videos_info = JSON.parse(File.read(input))
+        playlist_videos_info['items'].each do |item|
+          video_id = item['id']
+          get_comments_process(service, video_id, output_dir)
+        end
+      # inputはYoutube url, Youtube video id
+      else
+        video_id =
+          if input =~ URI::DEFAULT_PARSER.make_regexp
+            URI.decode_www_form(URI(input).query).to_h['v']
+          else
+            input
+          end
+        get_comments_process(service, video_id, output_dir)
+      end
+    end
+
+    LOGGER.info { "elapsed time: #{elapsed_time} sec" }
+
+  rescue StandardError => e
+    LOGGER.error { "#{e.class}: #{e.message}\n#{e.backtrace.join("\n")}" }
   end
 end
