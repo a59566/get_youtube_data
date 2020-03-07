acb26cb45dff8d48b87044dfa5eacbf9bac59cfe
a59566 <a59566@outlook.com>
2020-03-07 12:37:02 +0800

    update command line options add allowed input add default output dir

--- get_playlist_videos_info.rb
+++ get_playlist_videos_info.rb
@@ -12,19 +12,27 @@ if $PROGRAM_NAME == __FILE__
   # パラメータの解析
   params = {}
   OptionParser.new do |opts|
-    opts.on('-i', '--input PLAYLIST_ID', String, 'Youtube playlist id')
-    opts.on('-o', '--output-dir [DIR]', String, 'Output directory')
+    opts.on('-i', '--input INPUT', String, 'Allow 2 types input', '1. youtube playlist url',
+            '2. youtube playlist id')
+    opts.on('-o', '--output-dir [DIR]', String, 'Output directory, default is playlist/')
   end.parse!(into: params)
-  play_list_id = params[:input]
-  output_dir = params[:"output-dir"]
+  input = params[:input]
+  output_dir = params[:"output-dir"] || 'playlist'
 
   # google api
   service = Google::Apis::YoutubeV3::YouTubeService.new
   service.key = File.read('youtube_api.key')
 
-  # 結果を取得して #{play_list_id}.json に書き込む
+  playlist_id =
+    if input =~ URI::DEFAULT_PARSER.make_regexp
+      URI.decode_www_form(URI(input).query).to_h['list']
+    else
+      input
+    end
+
+  # 結果を取得して #{output_dir}/#{play_list_id}.json に書き込む
   playlist_items = service.fetch_all do |token, s|
-    s.list_playlist_items('contentDetails, snippet', max_results: 50, playlist_id: play_list_id, page_token: token)
+    s.list_playlist_items('contentDetails, snippet', max_results: 50, playlist_id: playlist_id, page_token: token)
   end
 
   video_properties = { items: [] }
@@ -39,9 +47,9 @@ if $PROGRAM_NAME == __FILE__
     video_properties[:items].push(video_property)
   end
 
-  FileUtils.mkpath(output_dir) if output_dir
-  output_file = "#{play_list_id}.json"
-  output_path = output_dir ? File.join(output_dir, output_file) : output_file
+  FileUtils.mkpath(output_dir)
+  output_file = "#{playlist_id}.json"
+  output_path = File.join(output_dir, output_file)
 
   File.open(output_path, 'w:UTF-8') do |file|
     file.puts(JSON.pretty_generate(video_properties))
